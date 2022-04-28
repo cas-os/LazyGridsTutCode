cd0afb42267154a1b22618b04c6638167b1e347e
Kazimierz Osinski <kazimierz.osinski@gmail.com>
2022-04-28 18:26:31 +0200

    Initial Commit

---
+++ LazyGridsTutCode/LazyGridsTutCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyGridsTutCodeApp.swift
+//  LazyGridsTutCode
+//
+//  Created by HAL-9001 on 28/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct LazyGridsTutCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
