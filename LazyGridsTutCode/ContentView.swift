76afac04d98d6f7326fd4ba9225a56231183fcf2
Kazimierz Osinski <kazimierz.osinski@gmail.com>
2022-04-28 19:34:28 +0200

    .////

--- LazyGridsTutCode/ContentView.swift
+++ LazyGridsTutCode/ContentView.swift
@@ -10,23 +10,58 @@ import SwiftUI
 struct ContentView: View {
     
     let columns: [GridItem] = [
-        GridItem(.flexible(), spacing: nil, alignment: nil),
-        GridItem(.flexible(), spacing: nil, alignment: nil),
-        GridItem(.flexible(), spacing: nil, alignment: nil),
-        GridItem(.flexible(), spacing: nil, alignment: nil),
+        GridItem(.flexible(), spacing: 6, alignment: nil),
+        GridItem(.flexible(), spacing: 6, alignment: nil),
+        GridItem(.flexible(), spacing: 6, alignment: nil),
     ]
     
     var body: some View {
         ScrollView {
+//            Rectangle()
+//                .fill(Color.white)
+//                .frame(height: 240)
+//            LazyVGrid(columns: [GridItem(.fixed(50))], alignment: .center, spacing: nil, pinnedViews: [], content: {
             Rectangle()
-                .fill(Color.white)
-                .frame(height: 240)
-            LazyVGrid(columns: columns) {
-                ForEach(0..<50) { index in
-                    Rectangle()
-                        .frame(height: 115)
-                }
-            }
+                .fill(Color.orange)
+                .frame(height: 200)
+            
+            LazyVGrid(columns: columns, alignment: .center, spacing: 6, pinnedViews: [], content: {
+                Section(header:
+                            Text("Section 1")
+                            .foregroundColor(.white)
+                            .font(.title)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .background(Color.blue)
+                            .padding()
+                ) {
+                
+                    ForEach(0..<50) { index in
+                        Rectangle()
+                            .frame(height: 50)
+                    }}
+                
+                Section(header:
+                            Text("Section 2")
+                            .foregroundColor(.white)
+                            .font(.title)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .background(Color.red)
+                            .padding()
+                ) {
+                
+                    ForEach(0..<50) { index in
+                        Rectangle()
+                            .fill(Color.green)
+                            .frame(height: 50)
+                    }}
+            })
+
+//            LazyVGrid(columns: columns) {
+//                ForEach(0..<50) { index in
+//                    Rectangle()
+//                        .frame(height: 115)
+//                }
+//            }
         }
     }
 }
